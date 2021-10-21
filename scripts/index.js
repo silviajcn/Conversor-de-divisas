5c03fb5736beb9948f96ad0b4e45b89f46ca4304
Silvia <silviajcn@gmail.com>
2021-10-21 14:29:24 -0500

    Conversor de divisas

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+// alert('Validaciones del formulario');
+
+/*let div1 = Number(document.getElementById('divisas1'));
+/*const div2 = document.getElementById('divisas2');
+const cant = parseFloat(document.getElementById('cantidad').value);*/
+
+/*const cotizacion = document.getElementById('cotizador');
+
+function calculate() {
+    const moneda1 = div1.value;
+    const moneda2 = div2.value;
+}*/
+
+/*
+
+div1.addEventListener('', calculate);
+div2.addEventListener('', calculate);
+cant.addEventListener('input', calculate)*/
+
+let pCol = ;
+let pMex = ;
+let dolar = ;
+let libra = ;
+let euro = ;
+let cotizacion = 0;
+
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    num = document.getElementById('cantidad').value;
+
+
+
+
+
+
+
+
+})*/
